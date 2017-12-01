d12098b12d05f607be7cbb68078b4a0bdc335444
nfagan <nick@fagan.org>
2017-12-01 17:59:45 -0500

    update build script

--- build.py
+++ build.py
@@ -1,12 +1,34 @@
 import subprocess
 import os
+import sys
+import shutil
 
+command_options = ['--rebuild']
 is_windows = os.name == 'nt'
 exp_dir = os.path.dirname(os.path.realpath(__file__))
+remove_build_dir = False;
+
+def get_help_text():
+	help_string = '\n\nValid options are:'
+	for i in xrange(len(command_options)):
+		help_string += ('\n\t"{0}"'.format(command_options[i]))
+	return help_string
+
+if len(sys.argv) > 1:
+	for i in xrange(len(sys.argv)-1):
+		if sys.argv[i+1] == '--rebuild':
+			remove_build_dir = True
+		else:
+			raise Exception('\n\nUnrecognized command "{0}"{1}\n'.format(
+				sys.argv[i+1], get_help_text()))
 
 def require_dir(directory):
 	if not os.path.exists(directory):
 		os.makedirs(directory)
+
+def conditional_remove_dir(directory, condition):
+	if os.path.exists(directory) and condition:
+		shutil.rmtree(directory)
 
 def get_generator_commands(directory):
 	generator_commands = ['cmake', '-G']
@@ -14,7 +36,6 @@ def get_generator_commands(directory):
 		generator_commands.append('Visual Studio 15 2017 Win64')
 	else:
 		generator_commands.append('Xcode')
-
 	generator_commands.append(directory)
 	return generator_commands
 
@@ -25,6 +46,7 @@ def build_subdirectory(name):
 	print('\n\nBuilding {0} ...'.format(name))
 	cmake_dir = os.path.join(exp_dir, name)
 	build_dir = os.path.join(cmake_dir, 'build')
+	conditional_remove_dir(build_dir, remove_build_dir)
 	require_dir(build_dir)
 	os.chdir(build_dir)
 	generator_commands = get_generator_commands(cmake_dir)
@@ -47,5 +69,6 @@ def build():
 	for i in xrange(len(modules)):
 		if check_errors(build_subdirectory(modules[i])):
 			return
+	print('\n\nAll succeeded.\n\n')
 
 build()
